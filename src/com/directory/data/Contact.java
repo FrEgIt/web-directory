ef5d7b86662b3c7a427cba5df3d41f9f4e4e1035
FrEgIt <mohamed.abdelnabi.formation@gmail.com>
2018-07-01 17:52:12 +0200

    Add data entity contact same as table's contact

---
+++ src/com/directory/data/Contact.java
@@ -0,0 +1,109 @@
+/**
+ * Contact.java
+ */
+package com.directory.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Mohamed ABDELNABI
+ *
+ */
+@Entity
+@Table
+public class Contact {
+
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String mail;
+
+	/**
+	 * @return the id
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_contact")
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	@Column(name = "first_name", nullable = false)
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	@Column(name = "last_name", nullable = false)
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	@Column(name = "phone_number", nullable = false)
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber
+	 *            the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the mail
+	 */
+	@Column(name = "mail", nullable = false)
+	public String getMail() {
+		return this.mail;
+	}
+
+	/**
+	 * @param mail
+	 *            the mail to set
+	 */
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+}
